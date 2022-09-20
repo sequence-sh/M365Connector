a436f8106c770de2253c74213ad4b4857df105a5
Mark Wainwright <wainwrightml@gmail.com>
2022-09-20 12:19:07 +0100

    Added Error case for M365Login

--- Microsoft365.Tests/Steps/M365LoginTests.cs
+++ Microsoft365.Tests/Steps/M365LoginTests.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using CSharpFunctionalExtensions;
 using Reductech.Sequence.Connectors.Microsoft365.Steps;
 using Reductech.Sequence.Core;
 using Reductech.Sequence.Core.Internal;
+using Reductech.Sequence.Core.Internal.Errors;
 using Reductech.Sequence.Core.Steps;
 using Reductech.Sequence.Core.Util;
+using Entity = Reductech.Sequence.Core.Entity;
 
 namespace Reductech.Sequence.Connectors.Microsoft365.Tests.Steps;
 
@@ -16,6 +19,31 @@ public partial class
         get
         {
             yield break;
+        }
+    }
+
+    /// <inheritdoc />
+    protected override IEnumerable<ErrorCase> ErrorCases
+    {
+        get
+        {
+            yield return new ErrorCase(
+                "No Settings",
+                new M365Login(),
+                ErrorCode.MissingStepSettings.ToErrorBuilder(
+                    "Reductech.Sequence.Connectors.Microsoft365"
+                )
+            );
+
+            //yield return new ErrorCase(
+            //    "Too many properties set",
+            //    new M365Login()
+            //    {
+            //        HandleLogin = new LambdaFunction<Entity, Unit>(null, new DoNothing()),
+            //        Token       = new SCLConstant<StringStream>("TestToken")
+            //    },
+            //    ErrorCode.ConflictingParameters.ToErrorBuilder("Token", "HandleLogin")
+            //) { StepFactoryStoreToUse = Maybe<StepFactoryStore>.From(IntegrationTestSettingsSFS) };
         }
     }
 
